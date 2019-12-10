3c0c1a09cc41c84680e24066257ce15de3678f7f
maerli <maerli.pereira@gmail.com>
2019-12-10 10:22:12 -0300

    adding routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React,{Component} from 'react';
+import './App.css'
+import { BrowserRouter,Switch, Route} from 'react-router-dom'
+import Home from './comp/Home'
+import Sobre from './comp/Sobre'
 
-function App() {
+export default class App extends Component{
+render(){
   return (
-    <div className="App">
-      
-    </div>
-  );
-}
 
-export default App;
+<BrowserRouter>
+    <div className="App">
+      <ul>
+	  <li><a className="active" href="/home">Home</a></li>
+	  <li><a href="/users">Usuários</a></li>
+	  <li><a href="/mesas">Mesas</a></li>
+	  <li><a href="/sobre">Sobre</a></li>
+</ul>
+</div>
+<Switch>
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/sobre" component={Sobre} />
+</Switch>
+</BrowserRouter>
+  )
+}
+}
